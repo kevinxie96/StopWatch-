db498656d43762e7bf2aa6d4ba4f704d7f03adc8
Howard Huang <howardhuang96@gmail.com>
2016-01-22 21:54:01 -0800

    clock working

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//clock
+window.onload = function() {
+  startTime();
+};
+
+
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('itemController', ['$scope', function($scope) {
+  //testing...
+  $scope.testing = 'wassup!';
+  //array of times
+  $scope.times = [];
+  //add time
+  $scope.add = function() {
+      time = document.getElementById('time').innerHTML
+      $scope.times.push(time);
+      $scope.input = '';
+  };
+    // remove an item
+  $scope.remove = function(index) {
+    $scope.times.splice(index, 1);
+  };
+}]);
