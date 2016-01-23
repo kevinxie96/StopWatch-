5ba4b890f58f8ce2063f94c58b001210839ae3f8
kevinxie96 <kevinxie96@g.ucla.edu>
2016-01-23 04:20:55 -0800

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,38 @@
+function startTimea() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var mi = today.getMilliseconds();
+    h = checkTime (h);
+    m = checkTime(m);
+    s = checkTime(s);
+    mi = checkTimemi(mi);
+     document.getElementById('starttime').innerHTML =  h + ":" + m + ":" + s + ":" + mi;
+}
+
 function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
+    var mi = today.getMilliseconds();
+    h = checkTime (h);
     m = checkTime(m);
     s = checkTime(s);
+    mi = checkTimemi(mi);
     document.getElementById('time').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+    h + ":" + m + ":" + s + ":" + mi;
+
+    var t = setTimeout(startTime, 1);
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+function checkTimemi(i) {
+    if ( i < 10) { i = "0" + "0" + i};
+    if ( 10<= i < 100) {i = "0" + i};  // add zero in front of numbers < 10
+
     return i;
 }
