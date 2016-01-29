dd25dbd18102b05eb0e120ef4d6cf5e2908dfd43
kevinxie96 <kevinxie96@g.ucla.edu>
2016-01-29 01:24:29 -0800

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,27 +1,59 @@
+var h1,m1,s1,mi1,h,m,s,mi;
 
-function startTime() {
+function reference() {
     var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    var mi = today.getMilliseconds();
-    h = checkTime (h);
-    m = checkTime(m);
-    s = checkTime(s);
-    mi = checkTimemi(mi);
-    document.getElementById('time').innerHTML =
-    h + ":" + m + ":" + s + ":" + mi;
-
-    var t = setTimeout(startTime, 1);
+    h1 = today.getHours();
+    m1 = today.getMinutes();
+    s1 = today.getSeconds();
+    mi1 = today.getMilliseconds();
+  
+     document.getElementById('starttime').innerHTML =  checkhms(h1) + ":" + checkhms(m1) + ":" + checkhms(s1) + ":" + checkmilli(mi1);
 }
 
-function checkTime(i) {
+
+var milliseconds = 0;
+var seconds = 0;
+
+
+function elapsed(){
+
+  if (milliseconds>=9){
+     milliseconds=0
+     seconds+=1
+  }
+  else
+     milliseconds+=1
+     document.d.d2.value = seconds + "." + milliseconds;
+     var timer = setTimeout(elapsed, 100);
+  }
+
+
+function startTime() {
+    
+
+    var today = new Date();
+    h = today.getHours();
+    m = today.getMinutes();
+    s = today.getSeconds();
+    mi = today.getMilliseconds();
+    
+    document.getElementById('time').innerHTML =
+    checkhms (h) + ":" + checkhms(m) + ":" + checkhms(s) + ":" + checkmilli(mi);
+
+    var t = setTimeout(startTime, 1);
+
+    
+}
+
+
+function checkhms(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-function checkTimemi(i) {
-    if ( i < 10) { i = "0" + "0" + i};
-     if (i < 100 && i >=10) {i = "0" + i}; // add zero in front of numbers < 10
-
+function checkmilli(i) {
+    if (i < 10) { i = "0" + "0" + i};
+    if ( i >= 10 && i < 100) { i = "0" + i};
+    // add zero in front of numbers < 10
+ 
     return i;
 }
